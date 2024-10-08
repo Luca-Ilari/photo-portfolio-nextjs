f64f75c56a901dbec9c4e0b0ddd0b1c1600df825
Luca Ilari <luca.ilari@gmail.com>
2024-10-08 16:40:37 +0200

    #1 #4 added gallery title in home

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { galleryProperty, getFolders } from "@/actions/actions";
+import {galleryProperty, getFolders} from "@/actions/actions";
 
 export default async function Home() {
     const folders: galleryProperty[] = await getFolders();
@@ -13,16 +13,22 @@ export default async function Home() {
                 <div className="gallery-wrap">
                     {folders.map((v) => {
                         return (
-                            <div className="item item-1" key={v.preview_image}>
+                            <div className="item" key={v.preview_image}>
                                 <Link key={v.path} href={v.path}>
                                     <Image
                                         src={"/" + v.preview_image}
                                         fill
-                                        style={{ objectFit: "cover" }}
+                                        style={{objectFit: "cover"}}
                                         alt=""
                                         quality={100}
                                     />
                                 </Link>
+                                <p style={{
+                                    position: "absolute",
+                                    bottom: 10,
+                                    width: "100%",
+                                    textAlign: "center"
+                                }}>{v.title}</p>
                             </div>
                         );
                     })}
